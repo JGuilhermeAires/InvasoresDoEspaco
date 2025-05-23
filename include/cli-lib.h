aa088b9e50a9cfc689760f1df6656b72c9caf72c
Bruno-h-Braga <brunoh0400@gmail.com>
2025-05-23 18:49:31 -0300

    Update cli-lib.h

--- include/cli-lib.h
+++ include/cli-lib.h
@@ -7,6 +7,5 @@ void cli_clear_screen();
 void cli_draw_char(int x, int y, char c);
 void cli_draw_string(int x, int y, const char* s);
 void cli_draw_int(int x, int y, int value);
-void cli_flush(); 
-
+void cli_flush();
 #endif
