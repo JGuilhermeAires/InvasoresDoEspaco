5c4575a62a4f7057cf4cfe3ae973120441500cd2
Bruno-h-Braga <brunoh0400@gmail.com>
2025-05-23 18:50:43 -0300

    Update cli-lib.c

--- src/cli-lib.c
+++ src/cli-lib.c
@@ -1,1 +1,41 @@
-aqa
+#include "cli-lib.h"
+#include <ncursesw/ncurses.h> 
+#include <string.h>  
+#include <stdio.h>   
+
+void cli_init() {
+    initscr();             
+    cbreak();              
+    noecho();              
+    nodelay(stdscr, TRUE); 
+    keypad(stdscr, TRUE);  
+    curs_set(0);           
+}
+
+void cli_end() {
+    curs_set(1);           
+    endwin();              
+}
+
+void cli_clear_screen() {
+    clear();              
+}
+
+void cli_draw_char(int x, int y, char c) {
+    mvaddch(y, x, c);      
+}
+
+void cli_draw_string(int x, int y, const char* s) {
+    mvprintw(y, x, "%s", s); 
+}
+
+
+void cli_draw_int(int x, int y, int value) {
+    char buffer[20];
+    sprintf(buffer, "%d", value);
+    mvprintw(y, x, "%s", buffer);
+}
+
+void cli_flush() {
+    refresh();
+}
