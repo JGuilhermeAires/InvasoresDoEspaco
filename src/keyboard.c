e767a24a26d5f64b060c47aa3c7eeb42770eaffa
Bruno-h-Braga <brunoh0400@gmail.com>
2025-05-23 18:49:56 -0300

    Update keyboard.c

--- src/keyboard.c
+++ src/keyboard.c
@@ -1,33 +1,36 @@
+/**
+ * Keyboard.c
+ * Created on May, 19th 2025
+ * Author: Bruno Braga
+*/
 #include "keyboard.h"
-
 #include <ncursesw/ncurses.h> 
 
 PlayerAction get_player_action() {
-    int input = getch();
-                        
-    if (input != ERR) {
-        switch (input) {
+    int input = getch();                                     
+
+    if (input != ERR) { 
             case 'a':
             case 'A':
-            case KEY_LEFT:
+            case KEY_LEFT: 
                 return PLAYER_ACTION_MOVE_LEFT;
             
             case 'd':
             case 'D':
-            case KEY_RIGHT:
+            case KEY_RIGHT: 
                 return PLAYER_ACTION_MOVE_RIGHT;
             
-            case ' ':
+            case ' ': 
                 return PLAYER_ACTION_SHOOT;
             
             case 'q':
             case 'Q':
                 return PLAYER_ACTION_QUIT;
-            
+
             default:
-                return PLAYER_ACTION_NONE;
+                return PLAYER_ACTION_NONE; 
         }
     }
     
-    return PLAYER_ACTION_NONE;
+    return PLAYER_ACTION_NONE; 
 }
