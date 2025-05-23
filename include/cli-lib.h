45d93f91c4b5af37e5aacd4f5df64ec0f5a703b0
Bruno-h-Braga <brunoh0400@gmail.com>
2025-05-23 18:30:42 -0300

    Create cli-lib.h

---
+++ include/cli-lib.h
@@ -0,0 +1,12 @@
+#ifndef CLI_LIB_H
+#define CLI_LIB_H
+
+void cli_init();
+void cli_end();
+void cli_clear_screen();
+void cli_draw_char(int x, int y, char c);
+void cli_draw_string(int x, int y, const char* s);
+void cli_draw_int(int x, int y, int value);
+void cli_flush(); 
+
+#endif
